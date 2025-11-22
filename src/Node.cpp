ef386908f039e7e755ff0814a992038a4ce5b8e7
jaju0 <jannes_oltmanns@hotmail.de>
2025-11-22 11:05:01 +0100

    added Node class

---
+++ src/Node.cpp
@@ -0,0 +1,72 @@
+#include "Node.hpp"
+
+namespace a_star {
+
+Node::Node()
+    : m_x(0)
+    , m_y(0)
+    , m_walkable(true)
+    , m_gCost(0.0f)
+    , m_hCost(0.0f)
+    , m_fCost(0.0f)
+{
+
+}
+
+Node::Node(uint32_t x, uint32_t y, bool walkable)
+    : m_x(x)
+    , m_y(y)
+    , m_walkable(walkable)
+    , m_gCost(0.0f)
+    , m_hCost(0.0f)
+    , m_fCost(0.0f)
+{
+
+}
+
+bool Node::operator==(const Node& b) const
+{
+    return m_x == b.m_x && m_y == b.m_y;
+}
+
+void Node::setWalkable(bool walkable)
+{
+    m_walkable = walkable;
+}
+
+uint32_t Node::getX() const
+{
+    return m_x;
+}
+
+uint32_t Node::getY() const
+{
+    return m_y;
+}
+
+bool Node::isWalkable() const
+{
+    return m_walkable;
+}
+
+float Node::getGCost() const
+{
+    return m_gCost;
+}
+
+float Node::getHCost() const
+{
+    return m_hCost;
+}
+
+float Node::getFCost() const
+{
+    return m_fCost;
+}
+
+Node::SharedPtr Node::getParent() const
+{
+    return m_pParent;
+}
+
+}
---
+++ src/Node.hpp
@@ -0,0 +1,42 @@
+#ifndef A_STAR_NODE_HPP_
+#define A_STAR_NODE_HPP_
+
+#include <cstdint>
+#include <memory>
+
+namespace a_star {
+
+class Node
+{
+public:
+    using SharedPtr = std::shared_ptr<Node>;
+
+private:
+    uint32_t m_x;
+    uint32_t m_y;
+    bool m_walkable;
+    float m_gCost;
+    float m_hCost;
+    float m_fCost;
+    std::shared_ptr<Node> m_pParent;
+
+public:
+    Node();
+    Node(uint32_t x, uint32_t y, bool walkable);
+
+    bool operator==(const Node& b) const;
+
+    void setWalkable(bool walkable);
+
+    uint32_t getX() const;
+    uint32_t getY() const;
+    bool isWalkable() const;
+    float getGCost() const;
+    float getHCost() const;
+    float getFCost() const;
+    SharedPtr getParent() const;
+};
+
+}
+
+#endif
