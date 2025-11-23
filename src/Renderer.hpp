039689a4b5aeef2bf684bf5e7795f08cb51ffef0
jaju0 <jannes_oltmanns@hotmail.de>
2025-11-23 11:15:07 +0100

    made Renderer non copyable & added SharedPtr alias

--- src/Renderer.hpp
+++ src/Renderer.hpp
@@ -1,6 +1,7 @@
 #ifndef A_STAR_RENDERER_HPP_
 #define A_STAR_RENDERER_HPP_
 
+#include <memory>
 #include <vector>
 #include <SFML/Graphics.hpp>
 
@@ -10,6 +11,9 @@ namespace a_star {
 
 class Renderer
 {
+public:
+    using SharedPtr = std::shared_ptr<Renderer>;
+
 private:
     Grid::SharedPtr m_pGrid;
     std::shared_ptr<sf::RenderWindow> m_pRenderWindow;
@@ -21,6 +25,8 @@ private:
 
 public:
     Renderer(Grid::SharedPtr pGrid, float nodeSize);
+    Renderer(const Renderer&) = delete;
+    Renderer& operator=(const Renderer&) = delete;
 
     void init();
     void update();
