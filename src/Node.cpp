6dede45fcc2c8ac2ee1241aecc374bbe7cd41686
jaju0 <jannes_oltmanns@hotmail.de>
2025-11-23 18:25:41 +0100

    extended Node class for A* implementation

--- src/Node.cpp
+++ src/Node.cpp
@@ -6,6 +6,9 @@ Node::Node()
     : m_x(0)
     , m_y(0)
     , m_walkable(true)
+    , m_isPartOfPath(false)
+    , m_isOpen(false)
+    , m_isClosed(false)
     , m_gCost(0.0f)
     , m_hCost(0.0f)
     , m_fCost(0.0f)
@@ -17,6 +20,9 @@ Node::Node(uint32_t x, uint32_t y, bool walkable)
     : m_x(x)
     , m_y(y)
     , m_walkable(walkable)
+    , m_isPartOfPath(false)
+    , m_isOpen(false)
+    , m_isClosed(false)
     , m_gCost(0.0f)
     , m_hCost(0.0f)
     , m_fCost(0.0f)
@@ -26,7 +32,32 @@ Node::Node(uint32_t x, uint32_t y, bool walkable)
 
 bool Node::operator==(const Node& b) const
 {
-    return m_x == b.m_x && m_y == b.m_y;
+    return m_fCost == b.m_fCost;
+}
+
+bool Node::operator!=(const Node& b) const
+{
+    return m_fCost != b.m_fCost;
+}
+
+bool Node::operator<(const Node& b) const
+{
+    return m_fCost == b.m_fCost ? m_hCost < b.m_hCost : m_fCost < b.m_fCost;
+}
+
+bool Node::operator>(const Node& b) const
+{
+    return m_fCost == b.m_fCost ? m_hCost > b.m_hCost : m_fCost > b.m_fCost;
+}
+
+bool Node::operator<=(const Node& b) const
+{
+    return m_fCost <= b.m_fCost;
+}
+
+bool Node::operator>=(const Node& b) const
+{
+    return m_fCost >= b.m_fCost;
 }
 
 bool Node::equalCoords(const Node& b) const
@@ -37,6 +68,41 @@ bool Node::equalCoords(const Node& b) const
 void Node::setWalkable(bool walkable)
 {
     m_walkable = walkable;
+}
+
+void Node::isPartOfPath(bool isPartOfPath)
+{
+    m_isPartOfPath = isPartOfPath;
+}
+
+void Node::isOpen(bool isOpen)
+{
+    m_isOpen = isOpen;
+}
+
+void Node::isClosed(bool isClosed)
+{
+    m_isClosed = isClosed;
+}
+
+void Node::setGCost(float gCost)
+{
+    m_gCost = gCost;
+}
+
+void Node::setHCost(float hCost)
+{
+    m_hCost = hCost;
+}
+
+void Node::setFCost(float fCost)
+{
+    m_fCost = fCost;
+}
+
+void Node::setParent(SharedPtr parent)
+{
+    m_pParent = parent;
 }
 
 uint32_t Node::getX() const
@@ -52,6 +118,21 @@ uint32_t Node::getY() const
 bool Node::isWalkable() const
 {
     return m_walkable;
+}
+
+bool Node::isPartOfPath() const
+{
+    return m_isPartOfPath;
+}
+
+bool Node::isOpen() const
+{
+    return m_isOpen;
+}
+
+bool Node::isClosed() const
+{
+    return m_isClosed;
 }
 
 float Node::getGCost() const
--- src/Node.hpp
+++ src/Node.hpp
@@ -15,6 +15,9 @@ private:
     uint32_t m_x;
     uint32_t m_y;
     bool m_walkable;
+    bool m_isPartOfPath;
+    bool m_isOpen;
+    bool m_isClosed;
     float m_gCost;
     float m_hCost;
     float m_fCost;
@@ -26,13 +29,28 @@ public:
     Node(const Node&) = delete;
 
     bool operator==(const Node& b) const;
+    bool operator!=(const Node& b) const;
+    bool operator<(const Node& b) const;
+    bool operator>(const Node& b) const;
+    bool operator<=(const Node& b) const;
+    bool operator>=(const Node& b) const;
     bool equalCoords(const Node& b) const;
 
     void setWalkable(bool walkable);
+    void isPartOfPath(bool isPartOfPath);
+    void isOpen(bool isOpen);
+    void isClosed(bool isClosed);
+    void setGCost(float gCost);
+    void setHCost(float hCost);
+    void setFCost(float fCost);
+    void setParent(SharedPtr parent);
 
     uint32_t getX() const;
     uint32_t getY() const;
     bool isWalkable() const;
+    bool isPartOfPath() const;
+    bool isOpen() const;
+    bool isClosed() const;
     float getGCost() const;
     float getHCost() const;
     float getFCost() const;
